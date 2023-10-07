c08fb10be03ced8a0c967699e967d4b66dd34d4b
BrightkyEfoo <brightefoo@gmail.com>
2023-10-07 14:28:00 +0100

    loader add

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,6 +1,18 @@
-import React from "react";
-import './style.scss'
+import React, { useLayoutEffect } from "react";
+import "./style.scss";
+import { gsap } from "gsap";
 const Loading = () => {
+  useLayoutEffect(() => {
+    return () => {
+      const v = async () => {
+        const t1 = gsap.timeline();
+
+        await t1.to(".loader-container", { duration: 500, opacity: 0 });
+      };
+      v();
+      //   t1.play()
+    };
+  }, []);
   return (
     <div className="loader-container">
       <img src="images/coder.gif" alt="" />
